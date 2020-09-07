e2cc14b63127a31f1acd2b17c4f21537e1cc2f62
Mico Kontic <micokontic@gmail.com>
2020-09-07 20:34:54 +0200

    Updates

---
+++ precache-manifest.c1b0d191e6ac2543e061d7e8897be054.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a0e34dddba0839790df5717a0c2e739c",
+    "url": "/watchlist1/index.html"
+  },
+  {
+    "revision": "fbbe40d2afe754f2a626",
+    "url": "/watchlist1/static/css/main.4ff8ce74.chunk.css"
+  },
+  {
+    "revision": "970f8b5de0771fdee8b4",
+    "url": "/watchlist1/static/js/2.21e71a2b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/watchlist1/static/js/2.21e71a2b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fbbe40d2afe754f2a626",
+    "url": "/watchlist1/static/js/main.4a713578.chunk.js"
+  },
+  {
+    "revision": "177e4e56ff1fc2099de1",
+    "url": "/watchlist1/static/js/runtime-main.8d030a05.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/watchlist1/static/media/logo.5d5d9eef.svg"
+  }
+]);
